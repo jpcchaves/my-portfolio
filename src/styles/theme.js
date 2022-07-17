ee591db351eeda616b2eef7aedc8aa6f3133bcd0
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-17 13:27:12 -0300

    global theme created

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    bgColor: "#e9ecef",
+  },
+  fonts: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secondary: "'Arial', 'Helvetica', sans-serif",
+    },
+  },
+};
