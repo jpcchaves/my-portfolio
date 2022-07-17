9fc0d6e1e143d9070b8b35fea1a81d768f4c3673
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-17 15:42:49 -0300

    added components to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,14 @@
-import { HomeWrapper, Img, ImgWrapper } from "../../styles/Home.style";
+import {
+  Banner,
+  H1Banner,
+  Header,
+  HomeWrapper,
+  Img,
+  ImgWrapper,
+  P,
+  DownloadCurriculo,
+  ButtonDownload,
+} from "../../styles/Home.style";
 import BgImg from "../../assets/home-bg-img/header-bg-img.jpg";
 
 const Home = () => {
@@ -7,6 +17,14 @@ const Home = () => {
       <ImgWrapper>
         <Img src={BgImg} alt="Imagem de Fundo" />
       </ImgWrapper>
+      <Banner>
+        <H1Banner>Desenvolvedor FrontEnd</H1Banner>
+        <P>João Paulo</P>
+        <ButtonDownload>
+          <DownloadCurriculo>Download Currículo</DownloadCurriculo>
+        </ButtonDownload>
+        :
+      </Banner>
     </HomeWrapper>
   );
 };
--- src/styles/Home.style.js
+++ src/styles/Home.style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { theme } from "./theme";
 
-export const HomeWrapper = styled.section`
+export const HomeWrapper = styled.header`
   width: 100%;
   height: 100vh;
 `;
@@ -30,4 +31,79 @@ export const Img = styled.img`
       transform: scale(1);
     }
   }
+`;
+
+export const Banner = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 15%;
+`;
+
+export const H1Banner = styled.h1`
+  color: #fcfcfc;
+  font-size: 3rem;
+  font-weight: 200;
+  text-shadow: 0.2rem 0.3rem 2px rgba(0, 0, 0, 0.336);
+  line-height: 3.5rem;
+  margin-bottom: 2rem;
+  opacity: 0;
+  animation: moveBanner 0.5s forwards;
+
+  @keyframes moveBanner {
+    0% {
+      transform: translateY(10rem) rotateY(-30deg);
+    }
+
+    100% {
+      transform: translateY(0) rotateY(0);
+      opacity: 1;
+    }
+  }
+`;
+
+export const P = styled.p`
+  color: #fcfcfc;
+  font-size: 2rem;
+  text-shadow: 0.2rem 0.2rem rgba(0, 0, 0, 0.336);
+  margin-bottom: 2rem;
+  opacity: 0;
+  animation: moveBanner 0.7s forwards;
+
+  @keyframes moveBanner {
+    0% {
+      transform: translateY(10rem) rotateY(-30deg);
+    }
+
+    100% {
+      transform: translateY(0) rotateY(0);
+      opacity: 1;
+    }
+  }
+`;
+
+export const ButtonDownload = styled.button`
+  font-size: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  background: #00b4db;
+  background: -webkit-linear-gradient(to right, #0083b0, #00b4db);
+  background: linear-gradient(to right, #0083b0, #00b4db);
+  padding: 1rem 0.8rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  opacity: 0;
+  animation: moveBanner 2s forwards;
+
+  :hover {
+    opacity: 0.8;
+    color: black;
+    font-weight: bolder;
+  }
+`;
+
+export const DownloadCurriculo = styled.a`
+  text-decoration: none;
+  color: #fcfcfc;
 `;
