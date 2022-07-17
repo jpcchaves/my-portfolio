7976559609b34ded63175aaf58ebe0d7fcfaa45f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-17 12:25:39 -0300

    navbar finnished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+// CSS
 import "./App.css";
+// Components
+import Navbar from "./components/Navbar/Navbar";
+// Router
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Router path="/"/>
-        <Router path="/about" />
-        <Router path="/contact" />
-        <Router path="/skills" />
-        <Router path="/projects" />
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/contact" />
+        <Route path="/skills" />
+        <Route path="/projects" />
       </Routes>
     </Router>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+// Hooks
+import { useState } from "react";
+// Navbar Components
+import {
+  NavbarContainer,
+  LeftContainer,
+  RightContainer,
+  NavbarInnerContainer,
+  NavbarExtendedContainer,
+  NavbarLinkContainer,
+  NavbarLink,
+  OpenLinksButton,
+  NavbarLinkExtended,
+} from "../../styles/Navbar.style";
+
+const Navbar = () => {
+  // Navbar Hamburguer Hook
+  const [extendNavbar, setExtendNavbar] = useState(false);
+
+  return (
+    <NavbarContainer extendNavbar={extendNavbar}>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <NavbarLinkContainer>
+            <NavbarLink to="/">Home</NavbarLink>
+            <NavbarLink to="/about">About</NavbarLink>
+            <NavbarLink to="/skills">Skills</NavbarLink>
+            <NavbarLink to="/projects">Projects</NavbarLink>
+            <NavbarLink to="/contact">Contact</NavbarLink>
+            <OpenLinksButton
+              onClick={() => setExtendNavbar((currentValue) => !currentValue)}
+            >
+              {extendNavbar ? <>&#10005;</> : <>&#8801;</>}
+            </OpenLinksButton>
+          </NavbarLinkContainer>
+        </LeftContainer>
+        <RightContainer></RightContainer>
+      </NavbarInnerContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
+          <NavbarLinkExtended to="/about">About</NavbarLinkExtended>
+          <NavbarLinkExtended to="/skills">Skills</NavbarLinkExtended>
+          <NavbarLinkExtended to="/projects">Projects</NavbarLinkExtended>
+          <NavbarLinkExtended to="/contact">Contact</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
