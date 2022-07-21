e858f708a0294152975cc5f5e4c5e36ba34a6376
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-20 22:35:19 -0300

    updated home.style.js

--- src/styles/Home.style.js
+++ src/styles/Home.style.js
@@ -3,31 +3,34 @@ import styled from "styled-components";
 export const HomeWrapper = styled.header`
   width: 100%;
   height: 90vh;
+  overflow: hidden;
 `;
 
 export const ImgWrapper = styled.div`
+  overflow: hidden;
   width: 100%;
   height: 100%;
-  overflow: hidden;
   background-color: rgba(0, 0, 0, 0.8);
 `;
 
 export const Img = styled.img`
-  display: block;
+  object-fit: cover;
+  object-position: 60%;
   width: 100%;
   height: 100%;
   overflow: hidden;
-  object-fit: cover;
   opacity: 0.5;
-  animation: zoom 18s;
+  animation: zoom 15s;
 
   @keyframes zoom {
     0% {
-      transform: scale(1.8);
+      transform: scale(1.5);
+      object-position: 60%;
     }
 
     100% {
       transform: scale(1);
+      object-position: 80%;
     }
   }
 `;
